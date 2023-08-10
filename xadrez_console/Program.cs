690359fb5a7d6087d7c029a6c6ff5e24ed29ff31
Letícia S. Lenzoni <125830543+leticiascofield@users.noreply.github.com>
2023-08-10 09:58:43 -0300

    Base do projeto

---
+++ xadrez_console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace xadrez_console {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            
+
+        }
+    }
+
+}
